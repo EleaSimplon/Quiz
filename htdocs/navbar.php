272cfadefa0352af55f120364ad843c276610a4d
EleaSimplon <77976512+EleaSimplon@users.noreply.github.com>
2021-03-19 14:58:16 +0100

    Add files via upload

---
+++ htdocs/navbar.php
@@ -0,0 +1,28 @@
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nav Bar</title>
+
+    <!-- NAV BAR -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Quiz</a>
+            
+            <img src="img/Logo1.png" width="40vw">
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav">
+                        <a class="nav-link active" aria-current="page" href="page2/menu.php">Home</a>
+                        <a class="nav-link" href="page3/quiz.php">Play</a>
+                        <a class="nav-link" href="page3/score.php">Leader Board</a>
+                    </div>
+                </div>
+        </div>
+
+    </nav>
+</head>
